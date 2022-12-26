6a15fde3797f49a5c0475ca8aab93fe40c93eb5e
healingyou22 <eggi.tresnaf@gmail.com>
2022-12-27 02:52:59 +0700

    first commit

---
+++ resources/views/includes/script.blade.php
@@ -0,0 +1,8 @@
+<!-- JQuery JS -->
+<script src="{{ url('frontend/libraries/jquery/jquery-3.6.3.min.js') }}"></script>
+
+<!-- Bootstrap JS -->
+<script src="{{ url('frontend/libraries/bootstrap/js/bootstrap.js') }}"></script>
+
+<!-- Retina JS -->
+<script src="{{ url('frontend/libraries/retina/retina.min.js') }}"></script>
---
+++ resources/views/layouts/success.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+
+    {{-- CSS Styles --}}
+    @stack('prepend-style')
+    @include('includes.style')
+    @stack('addon-style')
+</head>
+
+<body>
+    {{-- Navbar --}}
+    @include('includes.navbar-alternate')
+
+    {{-- Content --}}
+    @yield('content')
+
+    {{-- Script JS --}}
+    @stack('prepend-script')
+    @include('includes.script')
+    @stack('addon-script')
+</body>
+
+</html>
