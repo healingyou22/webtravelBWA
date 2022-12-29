7a06079cb969995e4634a1fbc5d28c6ad916f997
healingyou22 <eggi.tresnaf@gmail.com>
2022-12-30 04:07:42 +0700

    CRUD Transaksi

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\GalleryController;
+use App\Http\Controllers\Admin\TransactionController;
 use App\Http\Controllers\Admin\TravelPackageController;
 use App\Http\Controllers\CheckoutController;
 use App\Http\Controllers\DetailController;
@@ -28,6 +29,7 @@ Route::prefix('admin')->middleware(['auth', 'admin'])->name('admin.')->group(fun
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
     Route::resource('travel-package', TravelPackageController::class);
     Route::resource('gallery', GalleryController::class);
+    Route::resource('transaction', TransactionController::class);
 });
 
 Auth::routes(['verify' => true]);
